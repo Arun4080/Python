237569913a4b42739e77d95b1f8420a6ce4cd747
Arun <arunv4080@gmail.com>
2018-05-29 08:56:29 +0530

    basics

---
+++ Revise Basics/List and its operations.py
@@ -0,0 +1,15 @@
+# List and operations
+
+a=[1,2,3,"Arun","Ashit"]
+b=["Verma","Kumar",4,5,6]
+
+a.append("Adish") #Add element in the last
+
+a.index("Arun") #check elements position
+
+a[2:] ; a[:3]; a[1:3] #slicing elements after 2
+
+a.reverse() #reversing order
+
+#print them for result
+
---
+++ Revise Basics/additon using function and input.py
@@ -0,0 +1,8 @@
+def addition():
+    a= int(input("Enter 1st no. "))
+    b= int(input("Enter 2nd no. "))
+    print("\nSum of numbers is " +str(a+b))
+
+addition()
+    
+    
