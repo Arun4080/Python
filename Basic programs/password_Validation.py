44f57e477bb9552da0daf815a0619f2809a8262d
Arun <arunv4080@gmail.com>
2021-05-05 08:23:03 +0530

    Update password_Validation.py

--- Basic programs/password_Validation.py
+++ Basic programs/password_Validation.py
@@ -1,5 +1,5 @@
 import re
-a='Arunverma@1.'
+a='yourPassword'
 x=True
 while x:
 	if (len(a)<6 or len(a)>16) or not re.search(r'[A-Z]', a) or not re.search(r'[a-z]',a) or not re.search(r'[$#@]',a) and re.search(r'/s',a):
